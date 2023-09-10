59b510b7d28a0e0f8d4a13cd95b226d694e3aaf2
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-09-10 20:34:27 +0600

    conditional css done

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './country.css'
 
 const Country = ({country}) => {
-   const {name,flags,population,area}=country
+   const {name,flags,population,area,cca3}=country
+   const [visited,setVisited]=useState(false)
+   const handleVisited=()=>{
+    setVisited(!visited);
+   }
     return (
-        <div className='country'>
-            <h3>Name: {name.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color:visited?'red':'black'}}>Name: {name.common}</h3>
             <img src={flags.png}/>
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p>Code: {cca3}</p>
+            <button onClick={handleVisited}>{visited?'Visited':'Going'}</button>
+            {visited && 'I have visited this country'}
         </div>
     );
 };
